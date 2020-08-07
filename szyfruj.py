66128fe7c4f72471a49415fdb712496a5c5a4a8b
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-08-07 13:29:12 +0200

    Update szyfruj.py

--- szyfruj.py
+++ szyfruj.py
@@ -1,8 +1,9 @@
 # coding=UTF-8
 # Program szyfrujący kontener plikowy i montujący go jako system plików
 
-import optparse, getpass, pyAesCrypt
+import optparse, getpass, pyAesCrypt, subprocess, os
 from os import stat, remove
+from sh import mount
 
 wer = "20.0806"
 bufferSize = 64 * 1024
@@ -21,9 +22,13 @@ parser.add_option('-d', '--decrypt',
     action="store", dest="decrypt",
     help="odszyfruje wskazany plik i zapisuje z rozszerzeniem .enc") 
 
+parser.add_option('-c', '--create-container',
+    action="store", dest="contsize",
+    help="tworzy i szyfruje kontener plikowy o wielkości <contsize>MB") 
+
 options, args = parser.parse_args()
 print(f'Program do szyfrowania i montowania plików.\n{options.query}')
-if (not options.encrypt) and (not options.decrypt):
+if (not options.encrypt) and (not options.decrypt) and (not options.contsize):
     print('\nNie podałeś nazwy pliku do zaszyfrowania ani odszyfrowania.\n')
     exit()
 
@@ -43,3 +48,17 @@ if (options.decrypt):
                 pyAesCrypt.decryptStream(fIn, fOut, haselko, bufferSize, encFileSize)
             except ValueError:
                 print("\nNieudane odszyfrowanie. Czy podałeś prawidłowe hasło?")
+#---------------------------------------------------------------------------------------------------------
+if (options.contsize):
+    with open('kontener.img', 'wb') as bigfile:
+        bigfile.seek(int(options.contsize) * 1024 * 1024 -1)
+        bigfile.write(b'0')
+        bigfile.seek(0)
+    with open("kontener.img", "rb") as fIn:
+        with open("kontener_enc.img", "wb") as fOut:
+            pyAesCrypt.encryptStream(fIn, fOut, haselko, bufferSize)
+    remove("kontener.img")
+    subprocess.check_call(["/sbin/mkfs.ext4", "./kontener_enc.img"])
+    os.system('/bin/mount -o loop ./kontener_enc.img /mnt/test')
+    # subprocess.check_call(["/bin/mount -o loop","./kontener_enc.img","/mnt/test"], shell=True)
+    # mount("./kontener_enc.img","/mnt/test", "-o loop")
