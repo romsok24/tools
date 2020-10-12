f92ba2642dbb35eab3ac87cfc96bde08a1c900eb
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-10-12 20:36:01 +0200

    Dodano potwierdzenie hasła

--- szyfruj_pliki.sh
+++ szyfruj_pliki.sh
@@ -5,22 +5,29 @@
 # ./szyfruj_pliki.sh ./dir-of-files-to-encrypt
 
 printf "Podaj haslo do szyfrowania: "
-read -s PASSPHRASE
+read -s PASSPHRASE1
+printf "\nPotwierdz haslo do szyfrowania: "
+read -s PASSPHRASE2
 
+if [ "$PASSPHRASE1" = "$PASSPHRASE2" ]; then
+echo
 echo "Przetwarzam $1"
 
-for file in $(ls $1*txt); do
+ls $1*txt | while read file;do
   file_name=${file::-4}
   enc_name="$file_name.enc"
 
   echo "Szyfruje $file"
 
   gpg \
-    --passphrase "$PASSPHRASE" \
+    --passphrase "$PASSPHRASE1" \
     --batch \
     --output "$file_name.enc" \
     --symmetric \
     --cipher-algo AES256 \
     "$file"
-
 done
+
+else
+  printf "\n\n[ERR] Wpowadzone hasla sie nie zgadzaja\n"
+fi
